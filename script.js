0b020e691a55fbee5f02dfa2bfbc4167c0b66a75
Mateus <msouza954@gmail.com>
2023-06-06 16:36:57 -0300

    aula

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-document
-  .querySelector("button.fat")
-  .addEventListener("click", function () {
-    document.querySelector("#footer")
-      .classList
-      .toggle("hide")
-  })
+function onOff() {
+  document.querySelector("#modal")
+    .classList
+    .toggle("hide")
+
+  document
+    .querySelector("body")
+    .classList
+    .toggle("hideScroll")
+  document
+    .querySelector("#modal")
+    .classList
+    .toggle("addScroll")
+}
