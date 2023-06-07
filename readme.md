69e07cee903ca19329391fdf1517f9973e3b9f2b
Mateus <msouza954@gmail.com>
2023-06-07 18:06:59 -0300

    aula

--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 instalei o express
 instalei o nodemon
+instalei o nunjucks
--- server.js
+++ server.js
@@ -2,17 +2,67 @@
 const express = require("express")
 const server = express()
 
+const ideas = [
+  {
+    img: "https://cdn-icons-png.flaticon.com/128/8263/8263115.png",
+    title: "Cursos de Programção",
+    category: "Estudo",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae",
+    url: "https://github.com/",
+  },
+  {
+    img: "https://cdn-icons-png.flaticon.com/128/1896/1896290.png",
+    title: "Acamp's Shalom",
+    category: "Acampamento",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae",
+    url: "https://github.com/",
+  },
+  {
+    img: "https://cdn-icons-png.flaticon.com/128/7803/7803739.png",
+    title: "Exercícios",
+    category: "Saúde",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae",
+    url: "https://github.com/",
+  },
+  {
+    img: "https://cdn-icons-png.flaticon.com/128/2558/2558332.png",
+    title: "Fotos",
+    category: "Fotografía",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae",
+    url: "https://github.com/",
+  },
+]
+
 //configura arquivos estaticos:(css,scripts, imagens)
 server.use(express.static("public"))
+
+//confugutação do nunjucks
+const nunjucks = require("nunjucks")
+nunjucks.configure("views", {
+  express: server,
+  noCache: true,
+})
 
 //Criewi uima rota "/"
 // e capturei o pedido do cliente para responder
 server.get("/", function (req, res) {
-  return res.sendFile(__dirname + "/index.html")
+
+  const reversedIdeas = [...ideas].reverse()
+  let lastIdeas = [];
+  for (let idea of reversedIdeas) {
+    if (lastIdeas.length < 2) {
+      lastIdeas.push(idea)
+    }
+  }
+
+  return res.render("index.html", { ideas: lastIdeas })
 })
 
 server.get("/ideias", function (req, res) {
-  return res.sendFile(__dirname + "/ideias.html")
+
+  const reversedIdeas = [...ideas].reverse()
+
+  return res.render("ideias.html", { ideas: reversedIdeas})
 })
 
 //liguei meu servidor na porta 3000
